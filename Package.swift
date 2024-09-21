6769c16961f653ed2959e78acb94a11d6da6bf76
Taylor Holliday <wtholliday@gmail.com>
2024-09-20 17:11:31 -0700

    swift package

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "glm-spm",
+    products: [
+        .library(
+            name: "glm",
+            targets: ["glm"]),
+    ],
+    targets: [
+        .target(name: "glm", path: ".", sources: ["spm.cpp"], publicHeadersPath: "glm"),
+    ],
+    cxxLanguageStandard: .cxx20
+)
+
